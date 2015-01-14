12155e26225c59f162b522127cc22101eb30fe65
jstraits <ladyorenda@gmail.com>
2015-01-14 02:38:49 -0500

    Added rough JSONs

---
+++ js/resumeBuilder.js
@@ -0,0 +1,97 @@
+var bio = {
+	"name": "Jessica Straits",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "734-716-5279",
+		"email": "ladyorenda@gmail.com",
+		"github": "jstraits",
+		"twitter": "@ladyorenda",
+		"location": "Pinckney, Michigan"
+	},
+	"welcomeMessage": "text goes here",
+	"skills": [
+    "skill1", "skill2", "skill3"
+	],
+	"biopic": "images/pic.jpg"
+}
+
+var education = {
+	"schools": [
+	{
+		"name": "string",
+		"location": "string",
+		"degree": "string",
+		"major": "string",
+		"dates": "integer (graduation date)",
+		"url": "string"
+	},
+	{
+		"name": "string",
+		"location": "string",
+		"degree": "string",
+		"major": "array of strings",
+		"dates": "integer (graduation date)",
+		"url": "string"
+	}
+	]
+	
+	"onlineCourses": [
+	{
+		"title": "string",
+		"school": "string",
+		"date": "integer (date finished)",
+		"url": "string"
+	},
+	{
+		"title": "string",
+		"school": "string",
+		"date": "integer (date finished)",
+		"url": "string"
+	}
+	]
+    display: function taking no parameters
+}
+
+var work = {
+	"jobs": [
+	{
+		"employer": "string",
+		"title": "string",
+		"location": "string",
+		"dates": "string (works with a hyphen between them)",
+		"description": "string"
+	},
+	{
+		"employer": "string",
+		"title": "string",
+		"location": "string",
+		"dates": "string (works with a hyphen between them)",
+		"description": "string"
+	}
+	]
+	display: function taking no parameters
+}
+
+var projects = {
+	"projects": [
+	{
+		"title": "string",
+		"dates": "string (works with a hyphen between them)",
+		"description": "string",
+		"images": "array with string urls"
+	},
+	{
+		"title": "string",
+		"dates": "string (works with a hyphen between them)",
+		"description": "string",
+		"images": "array with string urls"
+	},
+	{
+		"title": "string",
+		"dates": "string (works with a hyphen between them)",
+		"description": "string",
+		"images": "array with string urls"
+	}
+	]
+    display: function taking no parameters
+}
